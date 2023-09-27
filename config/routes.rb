6b048f04a7c7ae151b02c703065b302d3d38f8de
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-09-27 22:21:02 +0900

    Add kanji controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
+  get '/kanjis/:id', to: 'kanjis#show'
 
-  # letter_opner_web
+  # letter_opener_web
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
---
+++ spec/requests/kanjis_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Kanjis", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
