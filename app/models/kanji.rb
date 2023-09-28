41f055d6d326d76f8db169c5c3444e9475fa2938
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-09-28 16:35:17 +0900

    Edit user's parameters

--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -1,6 +1,11 @@
 class Kanji < ProgramDataRecord
   self.primary_key = :id
+
   def self.refresh
     Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
+  end
+
+  def to_param
+    grapheme
   end
 end
