70760e813298243912b1eb077a1832ab08c67cc4
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-09-29 15:58:22 +0900

    Insert association into kanjis-vocabs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# kanjis = Kanji.all
+# kanjis.each do |kanji|
+#   vocabs = Vocab.where("defacto_orthography LIKE ?", "%#{kanji.grapheme}%")
+#   vocabs.each do |vocab|
+#     kanji.vocabs << vocab
+#   end
+# end
