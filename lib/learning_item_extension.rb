14ce04d3b31ae25fac047ec700d0a808f5dc18c5
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-10-06 11:25:54 +0900

    Refactor codes

--- lib/learning_item_extension.rb
+++ lib/learning_item_extension.rb
@@ -15,21 +15,19 @@ module LearningItemExtension
     Kanji.where(id: possibly_acquired_items.map(&:kanji_id)).order(:id)
   end
 
+  # :acquiredなkanjiをもとに、:possibly_acquiredなkanjiを推測する
+  # 現状、:acquiredなkanjiよりrankingが高いものはすべて習得済みと見なすこととする
   def presume_acquired_kanjis
     acquired_kanji_ids = where('status = ?', LearningItem.statuses[:acquired]).order(:id).map(&:kanji_id)
     lowest_ranking = Kanji.where(id: acquired_kanji_ids).map(&:ranking).max
     Kanji.where('ranking < ?', lowest_ranking).order(:id).each do |kanji|
       unless map(&:kanji_id).include?(kanji.id)
-        create(kanji_id: kanji.id, status: :possibly_acquired) unless acquired_kanji_ids.include?(kanji.id)
+        create!(kanji_id: kanji.id, status: :possibly_acquired) unless acquired_kanji_ids.include?(kanji.id)
         next
       end
 
-      status = LearningItem.find_by(kanji_id: kanji.id).status
-      if status == 'possibly_planned'
-        LearningItem.find_by(kanji_id: kanji.id).status = :possibly_acquired
-      else
-        next
-      end
+      item = LearningItem.find_by(kanji_id: kanji.id)
+      item.status = :possibly_acquired if item.status == 'possibly_planned'
     end
     possibly_acquired_kanjis
   end
