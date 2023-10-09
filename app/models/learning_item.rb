3178384573e9e9003eb39fe64c7d3941d09916f5
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-10-09 23:56:45 +0900

    Add method to create_or_update learning_item

--- app/models/learning_item.rb
+++ app/models/learning_item.rb
@@ -14,4 +14,10 @@ class LearningItem < ApplicationRecord
   belongs_to :kanji, foreign_key: :kanji_id
 
   enum status: { acquired: 1, possibly_acquired: 2, possibly_planned: 3, planned: 4 }
+
+  def self.create_or_update!(user_id:, kanji_id:, status:)
+    raise ArgumentError, "status is invalid: #{status.inspect}" unless LearningItem.statuses.include?(status)
+
+    find_by(user_id: user_id, kanji_id: kanji_id)&.update(status: status) || create!(user_id: user_id, kanji_id: kanji_id, status: status)
+  end
 end
